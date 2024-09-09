87f731eeb062c122d075b9807e55908ed175f630
rakshithvk19 <rakshithvk19@gmail.com>
2024-09-09 14:20:26 -0400

    chore: Added boilerplate code

---
+++ src/CSMM.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {BeforeSwapDelta, toBeforeSwapDelta} from "v4-core/types/BeforeSwapDelta.sol";
+import {CurrencySettler} from "@uniswap/v4-core/test/utils/CurrencySettler.sol";
+import {Currency} from "v4-core/types/Currency.sol";
+
+contract CSMM is BaseHook {
+    using CurrencySettler for Currency;
+
+    // error to throw when someone tries adding liquidity directly to the PoolManager
+    error AddLiquidityThroughHook();
+
+    constructor(IPoolManager poolManager) BaseHook(poolManager) {}
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: true, // Don't allow adding liquidity normally
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: true, // Override how swaps are done
+            afterSwap: false,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: true, // Allow beforeSwap to return a custom delta
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
+
+    function beforeAddLiquidity(address, PoolKey calldata, IPoolManager.ModifyLiquidityParams calldata, bytes calldata)
+        external
+        pure
+        override
+        returns (bytes4)
+    {
+        revert AddLiquidityThroughHook();
+    }
+
+    function addLiquidity(PoolKey calldata key, uint256 amountEach) external {
+        //TODO:
+    }
+
+    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata)
+        external
+        override
+        returns (bytes4, BeforeSwapDelta, uint24)
+    {
+        //TODO:
+    }
+}
